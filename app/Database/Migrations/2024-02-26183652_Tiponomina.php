e2eef2ff3992f58ef4d26c93460c077e38c8b1f2
HP <HP@192.168.0.113>
2024-02-26 17:56:22 -0700

    Cambio Tipo nomina

--- app/Database/Migrations/2024-02-26183652_Tiponomina.php
+++ app/Database/Migrations/2024-02-26183652_Tiponomina.php
@@ -1,52 +1,50 @@
-    <?php
+<?php
 
-    namespace App\Database\Migrations;
+namespace App\Database\Migrations;
 
-    use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\Migration;
 
-    class Tiponomina extends Migration
-    {
-    public function up()
-    {
-     // Tiponomina
-    $this->forge->addField([
-        'id'                    => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-'codigo'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'nombre'             => ['type' => 'varchar', 'constraint' => 256, 'null' => true],
-'direccion'             => ['type' => 'varchar', 'constraint' => 256, 'null' => true],
-'colonia'             => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
-'ciudad'             => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
-'idEmpresa'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'idSucursal'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'porcISN'             => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
-'entidadFederativa'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'cxcNom'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'cxpISN'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'cxcInfonavit'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'cxcIMSS'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'cxcFonacot'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'diasPeriodoNomina'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'maxDias'             => ['type' => 'int', 'constraint' => 11, 'null' => true],
-'codigoPostal'             => ['type' => 'varchar', 'constraint' => 5, 'null' => true],
-'riesgoPto'             => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
-'areaSalMin'             => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
-'ejecutivo'             => ['type' => 'varchar', 'constraint' => 512, 'null' => true],
-'ctaNom'             => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
-'NRP'             => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
-'porcRiesgoTrabajo'             => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
-'numSucBancaria'             => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
+class Tiponomina extends Migration {
 
-        'created_at'       => ['type' => 'datetime', 'null' => true],
-        'updated_at'       => ['type' => 'datetime', 'null' => true],
-        'deleted_at'       => ['type' => 'datetime', 'null' => true],
-    ]);
+    public function up() {
+        // Tiponomina
+        $this->forge->addField([
+            'id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'codigo' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'nombre' => ['type' => 'varchar', 'constraint' => 256, 'null' => true],
+            'direccion' => ['type' => 'varchar', 'constraint' => 256, 'null' => true],
+            'colonia' => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
+            'ciudad' => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
+            'idEmpresa' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'idSucursal' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'porcISN' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'entidadFederativa' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'cxcNom' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'cxpISN' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'cxcInfonavit' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'cxcIMSS' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'cxcFonacot' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'diasPeriodoNomina' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'maxDias' => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'codigoPostal' => ['type' => 'varchar', 'constraint' => 5, 'null' => true],
+            'riesgoPto' => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
+            'areaSalMin' => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
+            'ejecutivo' => ['type' => 'varchar', 'constraint' => 512, 'null' => true],
+            'ctaNom' => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
+            'NRP' => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
+            'porcRiesgoTrabajo' => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
+            'numSucBancaria' => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+            'deleted_at' => ['type' => 'datetime', 'null' => true],
+        ]);
 
-    $this->forge->addKey('id', true);
+        $this->forge->addKey('id', true);
 
-    $this->forge->createTable('tiponomina', true);
+        $this->forge->createTable('tiponomina', true);
     }
 
-    public function down(){
+    public function down() {
         $this->forge->dropTable('tiponomina', true);
-        }
     }
+}
