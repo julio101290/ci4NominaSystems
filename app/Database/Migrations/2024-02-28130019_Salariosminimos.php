c4bbd654b1538898f7eac566b32cd790aef70189
HP <HP@192.168.0.113>
2024-03-07 12:51:53 -0700

    Cambios Migraciones

--- app/Database/Migrations/2024-02-28130019_Salariosminimos.php
+++ app/Database/Migrations/2024-02-28130019_Salariosminimos.php
@@ -1,25 +1,27 @@
-    <?php
-    namespace App\Database\Migrations;
-    use CodeIgniter\Database\Migration;
-    class Salariosminimos extends Migration
-    {
-    public function up()
-    {
-     // Salariosminimos
-    $this->forge->addField([
-        'id'                    => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-'idEmpresa'             => ['type' => 'int', 'constraint' => 11, 'null' => false],
-'descripcion'             => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
-'importe'             => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+<?php
 
-        'created_at'       => ['type' => 'datetime', 'null' => true],
-        'updated_at'       => ['type' => 'datetime', 'null' => true],
-        'deleted_at'       => ['type' => 'datetime', 'null' => true],
-    ]);
-    $this->forge->addKey('id', true);
-    $this->forge->createTable('salariosminimos', true);
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Salariosminimos extends Migration {
+
+    public function up() {
+        // Salariosminimos
+        $this->forge->addField([
+            'id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'idEmpresa' => ['type' => 'int', 'constraint' => 11, 'null' => false],
+            'descripcion' => ['type' => 'varchar', 'constraint' => 128, 'null' => true],
+            'importe' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+            'deleted_at' => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('salariosminimos', true);
     }
-    public function down(){
+
+    public function down() {
         $this->forge->dropTable('salariosminimos', true);
-        }
     }
+}
